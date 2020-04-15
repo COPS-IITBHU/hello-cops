3920901b359bfdb8fcfba078e84ba90f89e6f68d
pavitra_ag <pavitra97agarwal@gmail.com>
2020-04-15 16:35:32 +0530

    love_u_3000

---
+++ C++/love_u_3000.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+#define inf 10000000000000
+#define int long long int
+#define hell (int)(1e9+7)
+
+using namespace std;
+
+void solve(){
+    cout<<"love-u-3000\n";
+}
+
+signed main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	int t;
+	//cin>>t;
+	t=1;
+	while(t--){
+		solve();
+	}
+	return 0;
+}
