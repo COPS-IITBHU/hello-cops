6da3759cf2e04ebd245296436a68023058093807
chouhan3nilesh21 <44453058+chouhan3nilesh21@users.noreply.github.com>
2020-10-01 01:23:59 +0530

    Create The one with hello world.cpp

---
+++ C++/The one with hello world.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+int a[]={72,101,108,108,111,32,87,111,114,108,100};
+for(int i=0;i<11;i++){
+	printf("%c",a[i]);
+}
+
+
+return 0;
+}
+
