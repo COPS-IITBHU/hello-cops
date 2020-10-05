2b0c87881f3d2163a6bcb74364c3507ca7217c51
Anticode_AV <54286277+Anmol272001@users.noreply.github.com>
2020-10-05 12:01:46 +0530

    Create Anmol Varshney(19054003).cpp

---
+++ C++/Anmol Varshney(19054003).cpp
@@ -0,0 +1,81 @@
+/*              _   _               _                     __      __ 
+    /\         | | (_)             | |                   /\ \    / / 
+   /  \   _ __ | |_ _  ___ ___   __| | ___              /  \ \  / /  
+  / /\ \ | '_ \| __| |/ __/ _ \ / _` |/ _ \            / /\ \ \/ /   
+ / ____ \| | | | |_| | (_| (_) | (_| |  __/           / ____ \  /    
+/_/    \_\_| |_|\__|_|\___\___/ \__,_|\___|          /_/    \_\/     
+                                             ______                  
+                                            |______|                
+*/ 
+ 
+#include<bits/stdc++.h> 
+#include <ext/pb_ds/assoc_container.hpp> 
+#include <ext/pb_ds/tree_policy.hpp> 
+ 
+using namespace __gnu_pbds; 
+using namespace std; 
+ 
+#define ll long long 
+#define pb push_back 
+#define ppb pop_back 
+#define pf push_front 
+#define ppf pop_front 
+#define pii pair<int,int> 
+#define vi vector<int> 
+#define vs vector<string> 
+#define vii vector<pii> 
+#define mi map<int,int> 
+#define mii map<pii,int> 
+ 
+#define x first 
+#define y second 
+ 
+#define rep(i,a,b) for(ll i=a;i<b;i++) 
+#define repr(i,a,b) for(ll i=b-1;i>=a;i--) 
+#define trav(a, x) for(auto& a : x) 
+#define sz(x) (int)((x).size()) 
+#define all(x) (x).begin(),(x).end() 
+ 
+#define mem1(a) memset(a,-1,sizeof(a)) 
+#define mem0(a) memset(a,0,sizeof(a)) 
+ 
+ 
+#define mod             1000000007 
+#define inf             1e18 
+#define hell 1000000000000000007LL 
+#define lbnd lower_bound 
+#define ubnd upper_bound 
+#define max(a,b) (a>b?a:b) 
+#define min(a,b) (a<b?a:b) 
+ 
+ 
+#define setbits(x)      __builtin_popcountll(x) 
+#define zrobits(x)      __builtin_ctzll(x) 
+#define ps(x,y)         fixed<<setprecision(y)<<x 
+#define mk(arr,n,type)  type *arr=new type[n]; 
+#define w(x)            int x; cin>>x; while(x--) 
+#define pqb             priority_queue<int> 
+#define pqs             priority_queue<int,vi,greater<int> > 
+#define ps(x,y)         fixed<<setprecision(y)<<x 
+ 
+/* For Debugging */ 
+#define DEBUG cerr<<"\n>>>I'm Here<<<\n"<<endl; 
+#define display(x) trav(a,x) cout<<a<<" ";cout<<endl; 
+#define what_is(x) cerr << #x << " is " << x << endl; 
+ 
+std::mt19937_64 rng(std::chrono::steady_clock::now().time_since_epoch().count()); 
+#define ordered_set tree<ll, null_type,less<ll>, rb_tree_tag,tree_order_statistics_node_update> 
+#define TIME cerr << "\nTime elapsed:" << setprecision(5) <<1000.0 * clock() / CLOCKS_PER_SEC << "ms\n"; 
+#define FAST ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0); 
+ 
+#define PI 3.141592653589793 
+#define N 100005 
+
+
+ 
+int main() 
+{ 
+cout<<"Hello COPS";
+return 0; 
+}  
+ 
