28e2ef527b09fb601e601d7e8b4bf9c10a8d2af5
unlash <asahai100@gmail.com>
2020-10-17 22:38:35 +0530

    Added my cpp file - Anshul Sahai

---
+++ C++/unlash.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#define ll long long
+#define vi vector<int>
+#define vll vector<ll>
+#define pii pair<int,int>
+#define pll pair<long long, long long>
+#define mp(x,y) make_pair(x,y)
+#define pb push_back
+#define rep(i,a,b) for(int i=a;i<b;++i)
+#define repa(i,a,b) for(int i=a;i<=b;++i)
+#define fr first
+#define sr second
+using namespace std;
+
+int main(){
+    std::ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    //cin>>t;
+    t=1;
+    while(t-->0){
+        cout<<"Hello COPS, It's Anshul here!"
+        
+    }
+    return 0;
+}
