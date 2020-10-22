6354b024bbe1ee6003959755781fc4d457b4c8dc
Manish Singh <57687267+astromanish@users.noreply.github.com>
2020-10-22 20:31:17 +0600

    Create 19124021.cpp

---
+++ C++/19124021.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+#define vl vector<ll>
+
+#define all(v) v.begin(), v.end()
+#define endl "\n"
+
+#define MOD 1e9 + 7
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cout<<"hello cops";
+    return 0;
+}
